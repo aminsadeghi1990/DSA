f272a9b2aa51662303b626695d202a69fa3b95f7
aminsadeghi1990 <aminsadeghi1990z@gmail.com>
2024-05-05 15:03:51 +0330

    tree completed

--- tree/bst.py
+++ tree/bst.py
@@ -8,6 +8,7 @@ class Node():
         self.leftchild = None
         self.rightchild = None
 
+
 class Tree():
     def __init__(self):
         self.root = None
@@ -17,7 +18,7 @@ class Tree():
             self.root = Node(value)
         else:
             current = self.root
-            while current!= None:
+            while current != None:
                 if value < current.value:
                     if current.leftchild == None:
                         current.leftchild = Node(value)
@@ -33,7 +34,7 @@ class Tree():
 
     def lookup(self, value):
         current = self.root
-        while current!= None:
+        while current != None:
             if value < current.value:
                 current = current.leftchild
             elif value > current.value:
@@ -41,15 +42,37 @@ class Tree():
             else:
                 return True
         return False
+
     def pre_order_traverse(self):
-        self.pre_order_traverse(self.root)
-        
-    def pre_order_traverse(self, current):
-        if current!= None:
-            print(current.value)
-            self.pre_order_traverse(current.leftchild)
-            self.pre_order_traverse(current.rightchild)
-            
+        self._pre_order_traverse(self.root)
+
+    def _pre_order_traverse(self, node):
+        if node == None:
+            return
+        print(node.value)
+        self._pre_order_traverse(node.leftchild)
+        self._pre_order_traverse(node.rightchild)
+
+    def post_order_traverse(self):
+        self._post_order_traverse(self.root)
+
+    def _post_order_traverse(self, node):
+        if node == None:
+            return
+        self._post_order_traverse(node.leftchild)
+        self._post_order_traverse(node.rightchild)
+        print(node.value)
+
+    def height(self):
+        return self._height(self.root)
+
+    def _height(self, node):
+        if node is None:
+            return -1
+        left_height = self._height(node.leftchild)
+        right_height = self._height(node.rightchild)
+        return 1 + max(left_height, right_height)
+
     def to_graph(self):
         G = nx.DiGraph()
         nodes = [self.root]
@@ -63,8 +86,8 @@ class Tree():
                 if node.rightchild is not None:
                     G.add_edge(node.value, node.rightchild.value)
                     nodes.append(node.rightchild)
-        return G  
-        
+        return G
+
 
 new_tree = Tree()
 new_tree.insert(10)
@@ -77,11 +100,13 @@ new_tree.insert(7)
 new_tree.insert(17)
 new_tree.insert(20)
 
-print(new_tree.lookup(20))
-print(new_tree.lookup(19))
-print(new_tree.lookup(13))
-print(new_tree.lookup(7))
-new_tree.pre_order_traverse(new_tree.root)
+print("Pre-order traversal:")
+new_tree.pre_order_traverse()
+print("####################################")
+print("Height of the tree:", new_tree.height())
+print("####################################")
+print("Post-order traversal:")
+new_tree.post_order_traverse()
 
 G = new_tree.to_graph()
 nx.draw(G, with_labels=True)
